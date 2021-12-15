93975b3d00afeae8503966f438315987dc39b400
EkaterinaKot2021 <kat20-06@tut.by>
2021-12-15 16:13:22 +0300

    new lesson3

---
+++ src/com/exlab/Main.java
@@ -0,0 +1,32 @@
+package com.exlab;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{7, 4, 9, 1, 5};
+
+
+        boolean isSorted = false;
+        int temp = 0;
+        while (!isSorted) {
+            isSorted = true;
+
+            for (int i = 1; i < array.length; i++) ;
+            int i = 1;
+                if (array [i] < array[i - 1]) {
+                    temp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = temp;
+
+                    isSorted = false;
+
+                    System.out.println(array [i]);
+                }
+
+
+            }
+
+
+
+        }
+    }
